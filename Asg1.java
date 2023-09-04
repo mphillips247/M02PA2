ccdfa15bc8214abcc0cad20d88d0f16b0564e01d
mphillips247 <132312022+mphillips247@users.noreply.github.com>
2023-09-03 21:12:27 -0500

    Add files via upload

--- Asg1.java
+++ Asg1.java
@@ -1,18 +1,18 @@
 public class Asg1 {
     public static void main(String[] args) {
-        // Create an object of Triangle class
+        // create an object of Triangle class
         Triangle my = new Triangle(1, 1.5, 1, "white", true);
 		
-        // Call method getArea() to get the area of Triangle
+        // call method getArea()
         System.out.println("Area of Triangle " + my.getArea());
 		
-        // Call method getPerimeter() to get the perimeter of Triangle
+        // call method getPerimeter()
         System.out.println("Perimeter of triangle " + my.getPerimeter());
 		
-        // Call getter method getColor()
+        // call method getColor()
         System.out.println("Color of the Triangle is " + my.getColor());
 		
-        // Call getter method isFilled()
+        // call method isFilled()
         System.out.println("Is triangle filled " + my.isFilled());
     }
 }
--- Triangle.java
+++ Triangle.java
@@ -1,23 +1,25 @@
 public class Triangle extends GeometricObject {
-
+//instance variable length 
     double side1;
     double side2;
     double side3;
-
+//default constructor
     public Triangle() {
         super();
+        //initialize values
         side1 = 1.0;
         side2 = 1.0;
         side3 = 1.0;
     }
-
+//take length, color, and filled
     public Triangle(double a, double b, double c, String color, boolean filled) {
         super(color, filled);
+        //initialize with provided values
         side1 = a;
         side2 = b;
         side3 = c;
     }
-
+//begin pulling lengths
     public double getSide1() {
         return side1;
     }
@@ -29,7 +31,7 @@ public class Triangle extends GeometricObject {
     public double getSide3() {
         return side3;
     }
-
+//calculate
     @Override
     public double getArea() {
         double s = (side1 + side2 + side3) / 2;
@@ -44,5 +46,6 @@ public class Triangle extends GeometricObject {
     @Override
     public String toString() {
         return "Triangle: side1 = " + side1 + ", side2 = " + side2 + ", side3 = " + side3 + ", " + super.toString();
+    //return information
     }
 }
